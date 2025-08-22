55a81a1a8e1290da59bd746e72a9123dbe9a6702
GYANENDRA8080 <gyanendra8080@gmail.com>
2025-08-22 10:33:58 +0530

    rest_api_final

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PostsView
+from .views import PostsView, post_detail
 
 urlpatterns = [
     path('posts/', PostsView),
+    path('details/<int:pk>', post_detail),
+
+
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Post
 from .serializers import PostSerializer
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 @csrf_exempt
@@ -21,3 +22,24 @@ def PostsView(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = PostSerializer(post)
+        return JsonResponse(serializer.data)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(post, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
